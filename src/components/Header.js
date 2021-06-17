0175ac0107246a0aacc85b95bef5f1d1746e5d88
DodoBey <dogukanyigiter@gmail.com>
2021-06-17 13:52:52 -0700

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,27 @@
 import { useEffect, useState } from "react";
 import '../scss/Header.scss';
+import MainLogo from "../images/Logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
-
+    return (
+        <div>
+            <div className="navBar">
+            <div className="mainLogo">
+                <img src={MainLogo} alt="" />
+            </div>
+            <div className="searchBar">
+                <input type="text" placeholder="Search"/>
+            </div>
+            <div className="navigateButtons">
+                <a href=""><FontAwesomeIcon icon={faHome} /></a>
+                <a href=""></a>
+                <a href=""></a>
+            </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header;
