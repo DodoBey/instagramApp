e52286476aceb3db456e74142b172157229354bd
DodoBey <dogukanyigiter@gmail.com>
2021-06-18 03:18:06 -0700

    fetch data is working, need to share datas to component, must fix the bootstrap modal issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,24 +4,39 @@ import MainLogo from "../images/Logo.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faArrowCircleUp, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { Container } from "react-bootstrap";
+import Modal from 'react-bootstrap/Modal';
+import Post from "./Post";
 
 const Header = () => {
+    const [modalShow, setModalShow] = useState(false);
+    
     return (
-        <Container>
-            <div className="navBar">
-                <div className="mainLogo">
-                    <img src={MainLogo} alt="" />
+        <>
+            <Container>
+                <div className="navBar">
+                    <div className="mainLogo">
+                        <img src={MainLogo} alt="" />
+                    </div>
+                    <div className="searchBar">
+                        <input type="text" placeholder="Search" />
+                    </div>
+                    <div className="navigateButtons">
+                        <a href=""><FontAwesomeIcon icon={faHome} /></a>
+                        <a href="" onClick={() => setModalShow(true)}><FontAwesomeIcon icon={faArrowCircleUp} /></a>
+                        <a href=""><FontAwesomeIcon icon={faUserCircle} /></a>
+                    </div>
                 </div>
-                <div className="searchBar">
-                    <input type="text" placeholder="Search" />
-                </div>
-                <div className="navigateButtons">
-                    <a href=""><FontAwesomeIcon icon={faHome} /></a>
-                    <a href=""><FontAwesomeIcon icon={faArrowCircleUp} /></a>
-                    <a href=""><FontAwesomeIcon icon={faUserCircle} /></a>
-                </div>
-            </div>
-        </Container>
+            </Container>
+            <Container>
+                <Modal
+                    size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+                >
+                    <Post />
+                </Modal>
+            </Container>
+        </>
     )
 }
 
--- src/context/context.js
+++ src/context/context.js
@@ -1,31 +1,45 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 const AuthContext = React.createContext();
 
 export const AuthContextProvider = (props) => {
   const [image, setImage] = useState();
   const [data, setData] = useState();
+  const [comment, setComment] = useState();
 
   const api = {
-    mainUrl: "https://dummyapi.io/data/api",
+    postUrl: "https://dummyapi.io/data/api/post?limit=100",
+    commentUrl: "https://dummyapi.io/data/post/",
     app_id: '60cbbeb27632200c16f6e7bc'
   }
 
-  useEffect(() => {
-    fetch(`${api.mainUrl}/user`, { header: { 'app-id': api.app_id } }).then(response => {
-      if (response.status !== 200) {
-        console.log(`Houston we have a problem! It's = ${response.status}`)
-      }
-      response.json().then(data => {
-        setData(data)
-      });
-    })
-      .catch((error) => {
-        console.log(`Houston, we still have a problem! It's = ${error}`)
-      })
-  }, [])
 
-  console.log(data)
+  // Fetch function for posts
+  useEffect(() => {
+    // fetch(`${api.mainUrl}${api.app_id}/user`).then(response => {
+    //   if (response.status !== 200) {
+    //     console.log(`Houston we have a problem! It's = ${response.status}`)
+    //   }
+    //   response.json().then(data => {
+    //     setData(data)
+    //   });
+    // })
+    //   .catch((error) => {
+    //     console.log(`Houston, we still have a problem! It's = ${error}`)
+    //   })
+    axios.get(`${api.postUrl}`, { headers: { 'app-id': api.app_id } })
+    .then(({data}) => setData(data))
+    .catch(console.error)
+  }, []);
+
+  // Fetch function for comments
+  useEffect(() => {
+    axios.get(`${api.commentUrl}{clickedimageid}/comment?limit=100`, { headers: { 'app-id': api.app_id } }) // Need to get id from clicked image
+    .then(({commentData}) => setComment(commentData))
+    .catch(console.error)
+  }, [])
+  console.log(data, comment)
 
   return (
     <AuthContext.Provider value={{ image: image, setImage: setImage }}>
@@ -34,3 +48,6 @@ export const AuthContextProvider = (props) => {
   );
 };
 export default AuthContext;
+
+
+// Need to send data as a provider
