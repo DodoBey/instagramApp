e42dbf9b6ed677005c4a05040733c1290e6cfb9e
yumi0127 <a13158y@gmail.com>
2021-06-18 14:37:29 -0700

    wip

--- src/context/context.js
+++ src/context/context.js
@@ -1,12 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
+
+const initialState = {
+  value: "",
+};
+const imageReducer = (state, action) => {
+  console.log(state.value);
+  switch (action.type) {
+    case "UPDATE":
+      return { ...state, value: [action.payload, ...state.value] };
+    case "DELETE":
+      return { initialState };
+    default:
+      return state;
+  }
+};
 
 const AuthContext = React.createContext();
 
 export const AuthContextProvider = (props) => {
-  const [image, setImage] = useState();
+  //const [image, setImage] = useState();
+
+  const [state, dispatchImage] = useReducer(imageReducer, initialState);
 
   return (
-    <AuthContext.Provider value={{ image: image, setImage: setImage }}>
+    <AuthContext.Provider
+      value={{ imageState: state.value, dispatchImage: dispatchImage }}
+    >
       {props.children}
     </AuthContext.Provider>
   );
