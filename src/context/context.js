db146a16d7ce0d134e36e4d23e5beb344f76142b
yumi0127 <a13158y@gmail.com>
2021-06-18 16:09:32 -0700

    finish postpage

--- src/context/context.js
+++ src/context/context.js
@@ -4,12 +4,11 @@ const initialState = {
   value: "",
 };
 const imageReducer = (state, action) => {
-  console.log(state.value);
   switch (action.type) {
     case "UPDATE":
       return { ...state, value: [action.payload, ...state.value] };
     case "DELETE":
-      return { initialState };
+      return { value: "" };
     default:
       return state;
   }
@@ -18,10 +17,7 @@ const imageReducer = (state, action) => {
 const AuthContext = React.createContext();
 
 export const AuthContextProvider = (props) => {
-  //const [image, setImage] = useState();
-
   const [state, dispatchImage] = useReducer(imageReducer, initialState);
-  const [image, setImage] = useState();
   const [data, setData] = useState();
 
   const api = {
